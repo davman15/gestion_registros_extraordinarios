2a49d41dc09630dc6d0be7eb474966672edcbfe9
davman15 <63202994+davman15@users.noreply.github.com>
2023-04-07 17:55:57 +0200

    CheckPoint antes de hacer el update de evento

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,7 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import { DateSelectArg, EventClickArg, CalendarOptions, EventInput, Calendar } from '@fullcalendar/core';
-import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction'
+import { CalendarOptions, EventInput } from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction'
 import esLocale from '@fullcalendar/core/locales/es';
 import { map } from 'rxjs';
 import listPlugin from '@fullcalendar/list';
@@ -10,6 +10,8 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import { FullCalendarComponent } from '@fullcalendar/angular';
 import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
 import { IEvento } from 'src/app/models/IEvento';
+import * as moment from 'moment';
+import 'moment-timezone';
 declare var $: any;
 
 @Component({
@@ -22,9 +24,11 @@ export class InicioComponent {
   constructor(private db: AngularFirestore, private formBuilder1: FormBuilder) { }
 
   formGroup1!: FormGroup;
+  campos = ['gastos', 'transporte', 'dietas', 'viajes', 'alojamiento'];
   modoEdicion: boolean = false;
   @ViewChild('calendario') calendario!: FullCalendarComponent;
   @ViewChild('formulario') form!: NgForm;
+
   idUsuario!: any;
   eventos: EventInput[] = [];
   coleccionEventos!: AngularFirestoreCollection<any>;
@@ -41,14 +45,11 @@ export class InicioComponent {
       hour: '2-digit',
       minute: '2-digit'
     },
-    eventClick: (clickInfo: any) => {
-      this.borrarEvento(clickInfo);
-    },
     eventDrop: (datos: any) => {
-      this.coleccionEventos.doc(datos.event.id).update({ fechaFin: datos.event.endStr, fechaInicio: datos.event.startStr })
+      this.actualizarFechas(datos);
     },
     eventResize: (datos: any) => {
-      this.coleccionEventos.doc(datos.event.id).update({ fechaFin: datos.event.endStr, fechaInicio: datos.event.startStr })
+      this.actualizarFechas(datos);
     },
     selectOverlap: false,
     droppable: true,
@@ -60,6 +61,17 @@ export class InicioComponent {
       right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
     }
   };
+
+  private actualizarFechas(datos: any) {
+    const fechaHoraInicio = `${datos.event.start.getHours().toString().padStart(2, '0')}:${datos.event.start.getMinutes().toString().padStart(2, '0')}`;
+    const fechaHoraFin = `${datos.event.end.getHours().toString().padStart(2, '0')}:${datos.event.end.getMinutes().toString().padStart(2, '0')}`;
+    this.coleccionEventos.doc(datos.event.id).update({
+      fechaFin: datos.event.endStr,
+      fechaInicio: datos.event.startStr,
+      fechaInicioHora: fechaHoraInicio,
+      fechaFinHora: fechaHoraFin
+    });
+  }
 
   ngOnInit(): void {
     //Sacamos el id del usuario del objeto del localStorage
@@ -83,16 +95,75 @@ export class InicioComponent {
 
     this.cargarItems();
     this.formularios();
-
+    this.captarActualizaciones();
   }
 
   ngAfterViewInit(): void {
     this.calendario.getApi().on('dateClick', (info) => {
       $('#modalCalendario1').modal('show');
-      $("#fechaInicio").val(info.dateStr);
-      $("#fechaFin").val(info.dateStr);
+      this.formGroup1.get('fechaInicio')!.setValue(info.dateStr);
+      this.formGroup1.get('fechaFin')!.setValue(info.dateStr);
     });
 
+    this.calendario.getApi().on('eventClick', (info) => {
+      const sub = this.coleccionEventos.doc(info.event.id).valueChanges().subscribe(data => {
+        $('#modalCalendario1').modal('show');
+        this.rellenarDatosFormulario(data);
+        sub.unsubscribe();
+      });
+    });
+  }
+
+  rellenarDatosFormulario(evento: any) {
+    for (let propiedad in evento) {
+      if (propiedad == 'fechaInicio' || propiedad == 'fechaFin') {
+        let fecha = moment(evento[propiedad]).format('YYYY-MM-DD');
+        const control = this.formGroup1.get(propiedad);
+        if (control) {
+          control.setValue(fecha);
+        }
+      }
+      else {
+        const control = this.formGroup1.get(propiedad);
+        if (control) {
+          control.setValue(evento[propiedad]);
+        }
+      }
+    }
+  }
+
+  formularios() {
+    this.formGroup1 = this.formBuilder1.group({
+      item: ['', Validators.required],
+      fechaInicio: ['', Validators.required],
+      fechaInicioHora: ['', Validators.required],
+      fechaFin: ['', Validators.required],
+      fechaFinHora: ['', Validators.required],
+      colorEvento: ['#000000'],
+      concepto: [''],
+      trayecto: [''],
+      gastos: [null, Validators.pattern(/^\d+(?:[,.]\d{2})?$/)],
+      transporte: [null, Validators.pattern(/^\d+(?:[,.]\d{2})?$/)],
+      dietas: [null, Validators.pattern(/^[0-9]*$/)],
+      viajes: [null, Validators.pattern(/^\d+(?:[,.]\d{2})?$/)],
+      alojamiento: [null, Validators.pattern(/^\d+(?:[,.]\d{2})?$/)],
+      km: [null, Validators.pattern(/^[0-9]*$/)],
+      total: [0 + "€"]
+    }, {
+      validator: this.validarFechas.bind(this)
+    });
+  }
+
+  validarFechas() {
+    if (!this.formGroup1) {
+      return null;
+    }
+    const fechaInicio = new Date(this.formGroup1.get('fechaInicio')!.value + 'T' + this.formGroup1.get('fechaInicioHora')!.value);
+    const fechaFin = new Date(this.formGroup1.get('fechaFin')!.value + 'T' + this.formGroup1.get('fechaFinHora')!.value);
+    if (fechaFin.getTime() < fechaInicio.getTime()) {
+      return { fechasInvalidas: true };
+    }
+    return null;
   }
 
   cerrarModal() {
@@ -100,6 +171,10 @@ export class InicioComponent {
     if (this.form) {
       this.form.reset();
     }
+
+    this.formGroup1.get('fechaInicioHora')!.setValue("");
+    this.formGroup1.get('fechaFinHora')!.setValue("");
+    this.formGroup1.get('total')!.setValue(0 + "€");
   }
 
   aniadirEvento(evento: IEvento) {
@@ -117,36 +192,30 @@ export class InicioComponent {
     }
   }
 
-  formularios() {
-    this.formGroup1 = this.formBuilder1.group({
-      item: ['', Validators.required],
-      fechaInicio: [new Date(), Validators.required],
-      fechaFin: [new Date(), Validators.required],
-      colorEvento: ['#000000'],
-      concepto: [''],
-      trayecto: [''],
-      gastos: [''],
-      transporte: [''],
-      dietas: [''],
-      viajes: [''],
-      alojamiento: [''],
-      km: [''],
-      total: ['0€']
-    });
-
-  }
-
   registrarEvento() {
-    let evento: any = Object.assign({}, this.formGroup1.value);
-    console.log(evento);
-
+    let evento: IEvento = Object.assign({}, this.formGroup1.value);
+    evento.fechaInicio = this.administrarFecha(evento.fechaInicio, evento.fechaInicioHora);
+    evento.fechaFin = this.administrarFecha(evento.fechaFin, evento.fechaFinHora);
     //Primero aqui en el cliente aplicar validaciones
     if (this.formGroup1.invalid) {
       this.formGroup1.markAllAsTouched();
       return;
     }
 
-    //this.aniadirEvento(evento);
+    this.aniadirEvento(evento);
+  }
+
+  administrarFecha(fecha: Date, hora: string): any {
+    let fechaTz;
+    if (hora.toString() == "") {
+      const cogerFecha = moment(fecha);
+      fechaTz = cogerFecha.tz('Europe/Madrid').format();
+    }
+    else {
+      const cogerFechaHora = moment(`${fecha} ${hora}`);
+      fechaTz = cogerFechaHora.tz('Europe/Madrid').format();
+    }
+    return fechaTz;
   }
 
   cargarItems() {
@@ -178,6 +247,22 @@ export class InicioComponent {
     this.arrayItems['Profesiones'] = 'Profesiones';
     this.arrayItems['Personas Visitadas'] = 'Personas Visitadas';
     this.arrayItems['Visitas Misioneras'] = 'Visitas Misioneras';
+  }
+
+  captarActualizaciones() {
+    for (let i = 0; i < this.campos.length; i++) {
+      this.formGroup1.get(this.campos[i])!.valueChanges.subscribe(() => this.actualizarTotal());
+    }
+  }
+
+  actualizarTotal() {
+    let total = 0;
+    for (let i = 0; i < this.campos.length; i++) {
+      const valorCampo = this.formGroup1.get(this.campos[i])!.value || 0;
+      const fieldValueNum = parseFloat(valorCampo.toString().replace(',', '.'));
+      total += fieldValueNum;
+    }
+    this.formGroup1.get('total')!.setValue(Number(total).toFixed(2) + "€");
   }
 
   public get f() {
